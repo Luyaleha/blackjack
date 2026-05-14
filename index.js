cd66c7dc0682225c5793f3cecae3f61f54514387
luyaleha <luyaleha@gmail.com>
2026-05-14 10:00:49 +0400

    add newcard button

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let firstcard = 10
 let secondcard= 11
-let sum = firstcard + secondcard + 4
+let sum = firstcard + secondcard 
 let hasblackjack = false
 let isalive =true
-
+let message=""
 let messageEl= document.getElementById("message-el")
 let sumEl= document.querySelector("#sum-el")
 let cardEl= document.querySelector("#card-el")
-function startgame(){ 
+function startgame(){
+    rendergame()
+}
+
+
+function rendergame(){ 
  cardEl.textContent="cards:" + firstcard + "" + secondcard   
  sumEl.textContent="sum:"+ sum   
-if (sum>20){
+if (sum<20){
 message="do you want to draw a new card ?"
 }else if(sum===21){
     message="you have got blackjack ?"
@@ -20,4 +25,9 @@ hasblackjack=true;
     isalive=false;
 }
 messageEl.textContent= message
+}
+function newcard(){
+    let card = 6
+    sum += card
+    rendergame()
 }
